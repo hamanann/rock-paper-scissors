2af9c9da61c173d9766f734029f19c337b16ad6f
hamanann <a.ahmed_1805@hotmail.com>
2022-10-15 01:48:22 +0200

    Add function to play round of rock-paper-scissors

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -8,3 +8,21 @@ function getComputerChoice() {
         return 'Scissors';
     }
 }
+
+
+function playRound(playerSelection, computerSelection) {
+    playerSelection = playerSelection.slice(0, 1).toUpperCase() + 
+        playerSelection.slice(1).toLowerCase();
+    computerSelection = computerSelection.slice(0, 1).toUpperCase() + 
+        computerSelection.slice(1).toLowerCase();
+    let result;
+    if (playerSelection === computerSelection) {
+        return "Draw!";
+    } else if (playerSelection === 'Rock' && computerSelection === 'Scissors'
+            || playerSelection === 'Paper' && computerSelection === 'Rock'
+            || playerSelection === 'Scissors' && computerSelection === 'Paper') {
+        return `You Win! ${playerSelection} beats ${computerSelection}`;
+    } else {
+        return `You Lose! ${computerSelection} beats ${playerSelection}`;
+    }
+}
