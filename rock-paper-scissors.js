8b2b9d0b58e4a477280440c97eaa79d335ae772a
hamanann <a.ahmed_1805@hotmail.com>
2022-10-15 01:26:55 +0200

    Add function to simulate computer player

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,1 +1,10 @@
-console.log("HI");
+function getComputerChoice() {
+    let choice = Math.random() * 3;
+    if (choice < 1) {
+        return 'Rock';
+    } else if (choice < 2) {
+        return 'Paper'
+    } else {
+        return 'Scissors';
+    }
+}
