5f23ff795a08e1c048421c737e1ebaea4a8d9e30
hamanann <a.ahmed_1805@hotmail.com>
2022-10-15 02:13:35 +0200

    Add function for playing 5 consecutive rounds

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -11,18 +11,39 @@ function getComputerChoice() {
 
 
 function playRound(playerSelection, computerSelection) {
-    playerSelection = playerSelection.slice(0, 1).toUpperCase() + 
-        playerSelection.slice(1).toLowerCase();
-    computerSelection = computerSelection.slice(0, 1).toUpperCase() + 
-        computerSelection.slice(1).toLowerCase();
+    playerSelection = capitalize(playerSelection);
+    
     let result;
     if (playerSelection === computerSelection) {
+        console.log("Draw!");
         return "Draw!";
     } else if (playerSelection === 'Rock' && computerSelection === 'Scissors'
             || playerSelection === 'Paper' && computerSelection === 'Rock'
             || playerSelection === 'Scissors' && computerSelection === 'Paper') {
-        return `You Win! ${playerSelection} beats ${computerSelection}`;
+        console.log(`You Win! ${playerSelection} beats ${computerSelection}`);
+        return `Win`;
     } else {
-        return `You Lose! ${computerSelection} beats ${playerSelection}`;
+        console.log(`You Lose! ${computerSelection} beats ${playerSelection}`);
+        return `Lose`;
     }
 }
+
+function capitalize(str) {
+    return str.charAt(0).toUpperCase() + str.slice(1);
+}
+
+function game() {
+    let scoreComputer = 0;
+    let scorePlayer = 0;
+    for (let i = 0; i < 5; i++) {
+        let playerChoice = prompt("Chose: rock, paper or scissors");
+        let result = playRound(playerChoice, getComputerChoice());
+        if (result === "Win") {
+            scorePlayer++;
+        } else if (result === "Lose") {
+            scoreComputer++;
+        }
+    }
+}
+
+game();
